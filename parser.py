8e27842e14cad842516c7d39b919eaee1f03219f
Sasha <akoroza@stuy.edu>
2019-03-18 15:13:02 -0400

    boxy

--- parser.py
+++ parser.py
@@ -14,7 +14,7 @@ The file follows the following format:
          torus: add a torus to the edge matrix -
                 takes 5 arguemnts (cx, cy, cz, r1, r2)
          box: add a rectangular prism to the edge matrix -
-              takes 6 arguemnts (x, y, z, width, height, depth)
+                takes 6 arguemnts (x, y, z, width, height, depth)
 
 
 	 circle: add a circle to the edge matrix -
@@ -47,7 +47,7 @@ The file follows the following format:
 
 See the file script for an example of the file format
 """
-ARG_COMMANDS = [ 'circle', 'bezier', 'hermite', 'line', 'scale', 'move', 'rotate', 'save' ]
+ARG_COMMANDS = [ 'circle', 'bezier', 'hermite', 'line', 'scale', 'move', 'rotate', 'save','box','sphere','torus']
 
 def parse_file( fname, edges, transform, screen, color ):
 
@@ -80,9 +80,10 @@ def parse_file( fname, edges, transform, screen, color ):
                       float(args[6]), float(args[7]),
                       step, line)
 
-        elif line == 'line':
-            #print 'LINE\t' + str(args)
+        elif line == 'box':
+            add_box(edges, float(args[0]), float(args[1]), float(args[2]),float(args[3]),float(args[4]),float(args[5]))
 
+        elif line == 'line':
             add_edge( edges,
                       float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), float(args[5]) )
